c424fc5d3cac131022a3c911b47362a80541ceca
myrfy001 <11474381+myrfy001@users.noreply.github.com>
2023-07-23 10:47:23 +0800

    basic for valid step 1

--- ex/handshake/testbench.v
+++ ex/handshake/testbench.v
@@ -11,6 +11,8 @@ module testbench;
     wire down_valid;
     reg down_ready;
 
+    reg [7:0] last_down_receive;
+
     assign up_data = data_cnt;
 
 
@@ -35,14 +37,22 @@ module testbench;
 
     initial begin
         clk = 0;
-        rst_n = 0;
-        # 2;
         rst_n = 1;
-        step_cnt = 0;
-        data_cnt = 0;
+        # 1;
+        rst_n = 0;
+        # 1;
+        rst_n = 1;
+        
     end
 
     always #1 clk = ~ clk;
+
+    always @ (negedge rst_n) begin
+        step_cnt = 0;
+        data_cnt = 0;
+        last_down_receive = 8'hx;
+        down_ready = 0;
+    end
 
     always @ (posedge clk) begin
         step_cnt <= step_cnt + 1;
@@ -54,15 +64,30 @@ module testbench;
             up_valid <= 1;
             down_ready <= 1;
         end else if (step_cnt < 20) begin
-            up_valid <= 1;
-            down_ready <= ~down_ready;
-        end else begin
             up_valid <= ~up_valid;
             down_ready <= 1;
+        end else begin
+            up_valid <= 1;
+            down_ready <= ~down_ready;
         end
 
         if (up_valid && up_ready) begin
             data_cnt <= data_cnt + 1;
+        end
+
+        if (down_valid && down_ready) begin
+            if (last_down_receive === 8'hx) begin
+                last_down_receive <= 0;
+                if (down_data != 0 ) begin
+                    $display("data out error\n");
+                    $finish;
+                end
+            end else if (last_down_receive + 1 != down_data) begin
+                $display("data out error\n");
+                $finish;
+            end else begin 
+                last_down_receive <= down_data;
+            end
         end
     end
 endmodule
--- ex/handshake/valid_proxy.v
+++ ex/handshake/valid_proxy.v
@@ -9,8 +9,24 @@ module valid_proxy(
     output wire down_valid
 );
 
-    assign down_data = up_data;
-    assign down_valid = up_valid;
+
+    reg [7:0] data;
+    reg valid;
+
+    assign down_data = data;
+    assign down_valid = valid;
     assign up_ready = down_ready;
+
+
+    // 最简单的打拍实现，对于下游可以连续接收的情况（下游ready一直为高）时可以正常工作，但是，一旦下游阻塞，则会丢数据
+    always @ (posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            data <= 0;
+            valid <= 0;
+        end else begin
+            data <= up_data;
+            valid <= up_valid;
+        end
+    end
 
 endmodule
